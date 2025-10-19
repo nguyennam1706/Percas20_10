b70b7c2d7b6f83948c8ee4d9eddb4768da27af9d
namnh <117700726+nguyennam1706@users.noreply.github.com>
2025-10-19 22:15:34 +0700

    init project

---
+++ js/myJs.js
@@ -0,0 +1,67 @@
+const textConfig = {
+  text3: "20/10 này, Chị em có muốn nhận quà không nhỉ :v",
+  text5: "Muốn nhận quà <3",
+  text6: "Không nhận đâu :<<",
+};
+
+$(document).ready(function () {
+  // process bar
+  setTimeout(function () {
+    $(".spinner").fadeOut();
+    $("#preloader").delay(350).fadeOut("slow");
+    $("body").delay(350).css({
+      overflow: "visible",
+    });
+  }, 600);
+
+  $("#text3").html(textConfig.text3);
+  $("#no").html(textConfig.text5);
+  $("#yes").html(textConfig.text6);
+
+  // switch button position
+  function switchButton() {
+    var audio = new Audio("sound/duck.mp3");
+    audio.play();
+    var leftNo = $("#no").css("left");
+    var topNO = $("#no").css("top");
+    var leftY = $("#yes").css("left");
+    var topY = $("#yes").css("top");
+    $("#no").css("left", leftY);
+    $("#no").css("top", topY);
+    $("#yes").css("left", leftNo);
+    $("#yes").css("top", topNO);
+  }
+  // move random button póition
+  function moveButton() {
+    var audio = new Audio("sound/Swish1.mp3");
+    audio.play();
+    if (screen.width <= 600) {
+      var x = Math.random() * 300;
+      var y = Math.random() * 500;
+    } else {
+      var x = Math.random() * 500;
+      var y = Math.random() * 500;
+    }
+    var left = x + "px";
+    var top = y + "px";
+    $("#no").css("left", left);
+    $("#no").css("top", top);
+  }
+
+  var n = 0;
+  $("#no").mousemove(function () {
+    if (n < 1) switchButton();
+    if (n > 1) moveButton();
+    n++;
+  });
+  $("#no").click(() => {
+    if (screen.width >= 900) switchButton();
+  });
+
+  // show popup
+  $("#yes").click(function () {
+    var audio = new Audio("sound/tick.mp3");
+    audio.play();
+    window.location = "https://cuongnobro.github.io/Baby/";
+  });
+});
